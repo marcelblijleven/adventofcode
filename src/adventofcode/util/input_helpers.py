b7ad43d6fd335579b688c1d52a9a7a579246a7c7
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-18 21:00:44 +0100

    feat: add function to retrieve input as string

--- src/adventofcode/util/input_helpers.py
+++ src/adventofcode/util/input_helpers.py
@@ -8,8 +8,13 @@ def get_input_for_day(year: int, day: int) -> List[str]:
     """
     Get the input for the year/day as list of strings
     """
-    input_file = os.path.join(ROOT_DIR, 'inputs', str(year), f'day{day:02}')
+    input_file = os.path.join(ROOT_DIR, 'inputs', str(year), f'day_{day:02}.txt')
     return _get_input(input_file)
+
+
+def get_input_for_day_as_str(year: int, day: int) -> str:
+    input_file = os.path.join(ROOT_DIR, 'inputs', str(year), f'day_{day:02}.txt')
+    return _read_file(input_file)
 
 
 def _read_lines(file_name) -> List[str]:
@@ -37,4 +42,5 @@ def _read_file(file_name) -> str:
     with open(file_name) as file:
         content = file.read()
 
+    content = content.rstrip('\n')
     return content
