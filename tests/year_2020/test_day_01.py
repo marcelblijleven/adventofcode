a0bb2396e79a4328ee4fafe7e2c772f867392839
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-18 21:08:34 +0100

    test: add 2020 day 1 - 4 tests

---
+++ tests/year_2020/test_day_01.py
@@ -0,0 +1,11 @@
+from adventofcode.year_2020.day_01 import part_one, part_two
+
+
+def test_part_one():
+    test_data = ['1721', '979', '366', '299', '675', '1456']
+    assert 514579 == part_one(test_data)
+
+
+def test_part_two():
+    test_data = ['1721', '979', '366', '299', '675', '1456']
+    assert 241861950 == part_two(test_data)
---
+++ tests/year_2020/test_day_02.py
@@ -0,0 +1,11 @@
+from adventofcode.year_2020.day_02 import part_one, part_two
+
+
+def test_part_one():
+    test_data = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+    assert 2 == part_one(test_data)
+
+
+def test_part_two():
+    test_data = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+    assert 1 == part_two(test_data)
