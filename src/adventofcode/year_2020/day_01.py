4670a6deff03d92920c0681178fe65d31e3c896f
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-18 21:10:27 +0100

    solution: add 2020 day 1 part one and two

---
+++ src/adventofcode/year_2020/day_01.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+@solution_timer(2020, 1, 1)
+def part_one(input_data: List[str]) -> int:
+    numbers = map(int, input_data)
+    seen = []
+
+    for number in numbers:
+        need = 2020 - number
+        if need in seen:
+            return need * number
+        seen.append(number)
+
+    raise SolutionNotFoundException(2020, 1, 1)
+
+
+@solution_timer(2020, 1, 2)
+def part_two(input_data: List[str]) -> int:
+    import math
+    from itertools import combinations
+
+    numbers = map(int, input_data)
+
+    for c in combinations(numbers, 3):
+        if sum(c) == 2020:
+            return math.prod(c)
+
+    raise SolutionNotFoundException(2020, 1, 2)
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2020, 1)
+    part_one(data)
+    part_two(data)
