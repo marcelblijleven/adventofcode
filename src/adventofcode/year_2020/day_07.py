24893c110e471ce8e10554db9fe53b2788f301b6
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-18 21:03:23 +0100

    chore: add inputs for 2018-2020

---
+++ src/adventofcode/year_2020/day_07.py
@@ -0,0 +1,6 @@
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+if __name__ == '__main__':
+    ...
