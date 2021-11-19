6a06298f2a9947364a8d44aeddb029973a9d499f
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-19 22:30:52 +0100

    solution: add 2015 day 3 part one and two

---
+++ src/adventofcode/year_2015/day_03.py
@@ -0,0 +1,74 @@
+from typing import List, Dict
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+GridType = Dict[tuple[int, int], int]
+
+
+def get_new_position(current: tuple[int, int], instruction: str) -> tuple[int, int]:
+    x, y = current
+    if instruction == '^':
+        y += 1
+    elif instruction == '>':
+        x += 1
+    elif instruction == 'v':
+        y -= 1
+    elif instruction == '<':
+        x -= 1
+    else:
+        raise ValueError(f'invalid instruction received: {instruction}')
+
+    return x, y
+
+
+@solution_timer(2015, 3, 1)
+def part_one(input_data: List[str]):
+    str_data = ''.join(input_data)
+    current = (0, 0)
+    grid: GridType = {current: 1}
+
+    for instruction in str_data:
+        current = get_new_position(current, instruction)
+
+        if current not in grid:
+            grid[current] = 1
+        else:
+            grid[current] += 1
+
+    return len(grid)
+
+
+def update_grid(santa_like: tuple[int, int], grid: GridType) -> GridType:
+    if santa_like not in grid:
+        grid[santa_like] = 1
+    else:
+        grid[santa_like] += 1
+
+    return grid
+
+
+@solution_timer(2015, 3, 2)
+def part_two(input_data: List[str]):
+    str_data = ''.join(input_data)
+    santa = (0, 0)
+    robo_santa = (0, 0)
+    grid: GridType = {(0, 0): 2}
+
+    for i, instruction in enumerate(str_data):
+        if i % 2 != 0:
+            santa = get_new_position(santa, instruction)
+            grid = update_grid(santa, grid)
+        else:
+            robo_santa = get_new_position(robo_santa, instruction)
+            grid = update_grid(robo_santa, grid)
+
+    return len(grid)
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2015, 3)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2015/test_day_03.py
@@ -0,0 +1,22 @@
+import pytest
+from adventofcode.year_2015.day_03 import part_one, part_two
+
+
+@pytest.mark.parametrize(['instructions', 'expected'], [
+    ('^v', 2),
+    ('^>v<', 4),
+    ('^v^v^v^v^v', 2)
+])
+def test_part_one(instructions, expected):
+    assert expected == part_one([instructions])
+
+
+@pytest.mark.parametrize(['instructions', 'expected'], [
+    ('^v', 3),
+    ('^>v<', 3),
+    ('^v^v^v^v^v', 11)
+])
+def test_part_two(instructions, expected):
+    assert expected == part_two([instructions])
+
+
