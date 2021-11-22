9860896a53e61c4dbee6385f5b44d748a97fef95
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-22 21:25:54 +0100

    test: add 2015 day 7, 8, 9 tests

---
+++ tests/year_2015/test_day_08_2015.py
@@ -0,0 +1,27 @@
+import pytest
+
+from adventofcode.year_2015.day_08_2015 import parse_line, get_hex_character, parse_line_part_two
+
+
+@pytest.mark.parametrize(['line', 'expected'], [
+    (r'""', 2),
+    (r'"abc"', 2),
+    (r'"aaa\"aaa"', 3),
+    (r'"\x27"', 5),
+])
+def test_parse_line(line, expected):
+    assert expected == parse_line(line)
+
+
+def test_get_hex_character():
+    assert '\'' == get_hex_character(r'\x27')
+
+
+@pytest.mark.parametrize(['line', 'expected'], [
+    (r'""', 6 - 2),
+    (r'"abc"', 9 - 5),
+    (r'"aaa\"aaa"', 16 - 10),
+    (r'"\x27"', 11 - 6),
+])
+def test_part_two(line, expected):
+    assert expected == parse_line_part_two(line)
