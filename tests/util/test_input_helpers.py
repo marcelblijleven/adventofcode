699883fa7d38123c8ed170f63511c22a523f0075
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-23 20:09:54 +0100

    test: add get input for day as string test

--- tests/util/test_input_helpers.py
+++ tests/util/test_input_helpers.py
@@ -2,7 +2,7 @@ import pytest
 import pytest_mock
 
 from adventofcode.util import input_helpers
-from adventofcode.util.input_helpers import _read_file, _get_input, get_input_for_day
+from adventofcode.util.input_helpers import _read_file, _get_input, get_input_for_day, get_input_for_day_as_str
 
 
 def test_get_input_for_day(mocker: pytest_mock.MockerFixture):
@@ -15,6 +15,20 @@ def test_get_input_for_day(mocker: pytest_mock.MockerFixture):
     data = get_input_for_day(2020, 1)
     assert ['c0ffee', 'cafe'] == data
     mock_get_input.assert_called_with('dir/inputs/2020/day_01.txt')
+
+    input_helpers.ROOT_DIR = original_root_dir
+
+
+def test_get_input_for_day_as_str(mocker: pytest_mock.MockerFixture):
+    mock_read_file = mocker.patch('adventofcode.util.input_helpers._read_file')
+    mock_read_file.return_value = 'c0ffee\ncafe'
+
+    original_root_dir = input_helpers.ROOT_DIR
+    input_helpers.ROOT_DIR = 'dir'
+
+    data = get_input_for_day_as_str(2020, 1)
+    assert 'c0ffee\ncafe' == data
+    mock_read_file.assert_called_with('dir/inputs/2020/day_01.txt')
 
     input_helpers.ROOT_DIR = original_root_dir
 
