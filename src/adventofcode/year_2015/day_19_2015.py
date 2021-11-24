fde0f1082adcc406ccc301712cca668296e8f1bc
Marcel Blijleven <marcelblijleven@gmail.com>
2021-11-24 21:21:33 +0100

    solution: add 2015 day 17 to 20

---
+++ src/adventofcode/year_2015/day_19_2015.py
@@ -0,0 +1,71 @@
+import re
+from typing import List, Dict
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+REPLACEMENT_PATTERN = re.compile(r'(\w+) => (\w+)')
+
+
+def get_replacements(input_data: List[str]) -> list[tuple[str, str]]:
+    replacements: List[tuple[str, str]] = []
+
+    # skip last two rows because it doesn't contain replacements
+    for line in input_data[:-2]:
+        found = REPLACEMENT_PATTERN.findall(line)
+        if len(found) > 0:
+            replacements.append(found[0])
+
+    return replacements
+
+
+def find_unique_replacements(replacements: list[tuple[str, str]], medicine: str) -> int:
+    unique_replacements = set()
+
+    for key, value in replacements:
+        for i, _ in enumerate(medicine):
+            if medicine[i:i + len(key)] == key:
+                replacement = medicine[:i] + value + medicine[i+len(key):]
+                unique_replacements.add(replacement)
+
+    return len(unique_replacements)
+
+
+def count_time(replacements: list[tuple[str, str]], medicine: str) -> int:
+    count = 0
+
+    while medicine != 'e':
+        for key, value in replacements:
+            if value in medicine:
+                medicine = medicine.replace(value, key, 1)
+                count += 1
+    return count
+
+
+@solution_timer(2015, 19, 1)
+def part_one(input_data: List[str]):
+    replacements = get_replacements(input_data)
+    answer = find_unique_replacements(replacements, input_data[-1])
+
+    if not answer:
+        raise SolutionNotFoundException(2015, 19, 1)
+
+    return answer
+
+
+@solution_timer(2015, 19, 2)
+def part_two(input_data: List[str]):
+    answer = count_time(get_replacements(input_data), input_data[-1])
+
+    if not answer:
+        raise SolutionNotFoundException(2015, 19, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2015, 19)
+    part_one(data)
+    part_two(data)
---
+++ src/adventofcode/year_2015/day_20_2015.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+from typing import List, Dict
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def most_presents_at_house(houses: Dict[int, int]) -> int:
+    presents = sorted(houses.values())
+
+    if len(presents) == 0:
+        return 0
+
+    return presents[-1]
+
+
+def visit_houses(elf_count: int, target: int):
+    houses = defaultdict(int)
+
+    for elf in range(1, target):
+        for house in range(elf, elf_count, elf):
+            houses[house] += elf * 10
+
+        if houses[elf] >= target:
+            return elf
+
+
+def visit_houses_part_two(elf_count: int, target: int):
+    houses = defaultdict(int)
+
+    for elf in range(1, target):
+        for house in range(elf, min([elf * 50 + 1, elf_count]), elf):
+            houses[house] += elf * 11
+
+        if houses[elf] >= target:
+            return elf
+
+
+@solution_timer(2015, 20, 1)
+def part_one(input_data: List[str]):
+    target = int(input_data[0])
+    answer = visit_houses(1000000, target)
+
+    if not answer:
+        raise SolutionNotFoundException(2015, 20, 1)
+
+    return answer
+
+
+@solution_timer(2015, 20, 2)
+def part_two(input_data: List[str]):
+    target = int(input_data[0])
+    answer = visit_houses_part_two(1000000, target)
+
+    if not answer:
+        raise SolutionNotFoundException(2015, 20, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2015, 20)
+    part_one(data)
+    part_two(data)
