d2dbbe7c2a53c6709d2449317d4596d17cbd1e30
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-01 11:30:23 +0100

    test: improve test coverage for 2015

--- tests/year_2015/test_day_02_2015.py
+++ tests/year_2015/test_day_02_2015.py
@@ -1,6 +1,7 @@
 import pytest
 
-from adventofcode.year_2015.day_02_2015 import get_surface, get_ribbon
+from adventofcode.year_2015.day_02_2015 import get_surface, get_ribbon, parse_line, part_one, get_input_for_day, \
+    part_two
 
 
 @pytest.mark.parametrize(['line', 'expected'], [
@@ -17,3 +18,18 @@ def test_get_surface(line, expected):
 ])
 def test_get_ribbon(line, expected):
     assert expected == get_ribbon(line)
+
+
+def test_parse_line():
+    with pytest.raises(ValueError) as wrapped_e:
+        parse_line('incorrect line')
+
+    assert str(wrapped_e.value) == 'could not parse line'
+
+
+def test_part_one():
+    assert part_one(get_input_for_day(2015, 2)) == 1606483
+
+
+def test_part_two():
+    assert part_two(get_input_for_day(2015, 2)) == 3842356
--- tests/year_2015/test_day_04_2015.py
+++ tests/year_2015/test_day_04_2015.py
@@ -1,5 +1,5 @@
 import pytest
-from adventofcode.year_2015.day_04_2015 import find_number
+from adventofcode.year_2015.day_04_2015 import find_number, part_one, part_two
 
 
 @pytest.mark.parametrize(['secret', 'target', 'range_size', 'expected'], [
@@ -8,3 +8,11 @@ from adventofcode.year_2015.day_04_2015 import find_number
 ])
 def test_find_number(secret, target, range_size, expected):
     assert expected == find_number(secret, target, range_size)
+
+
+def test_part_one():
+    assert part_one(['abcdef']) == 609043
+
+
+def test_part_two():
+    assert part_two(['pqrstuv']) == 5714438
