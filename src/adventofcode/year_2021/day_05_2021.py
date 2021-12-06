0be615b03cb1d8c0e227f60a2f69adbe65d6fc31
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-06 08:42:19 +0100

    solution: add 2021 day 5

---
+++ src/adventofcode/year_2021/day_05_2021.py
@@ -0,0 +1,100 @@
+import re
+from collections import defaultdict
+from typing import List, DefaultDict, Set
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+Coord = tuple[int, int]
+GridType = DefaultDict[Coord, int]
+LinePositions = tuple[Coord, Coord]
+Line = Set[Coord]
+line_pattern = re.compile(r'(\d+)')
+
+
+def is_horizontal(line: LinePositions) -> bool:
+    start, end = line
+    return start[0] == end[0]
+
+
+def is_vertical(line: LinePositions) -> bool:
+    start, end = line
+    return start[1] == end[1]
+
+
+def is_diagonal(line: LinePositions) -> bool:
+    return not is_vertical(line) and not is_horizontal(line)
+
+
+def get_line(positions: LinePositions) -> Line:
+    start, end = positions
+
+    x1, y1 = start
+    x2, y2 = end
+
+    dx = bool(x2 > x1) - bool(x2 < x1)
+    dy = bool(y2 > y1) - bool(y2 < y1)
+
+    return {(x1 + n * dx, y1 + n * dy) for n in range(max(abs(x2 - x1), abs(y2 - y1)) + 1)}
+
+
+def get_lines(positions_list: List[LinePositions]) -> List[Line]:
+    lines: List[Line] = []
+    for positions in positions_list:
+        lines.append(get_line(positions))
+
+    return lines
+
+
+def count_intersections(parsed_input: List[LinePositions]) -> int:
+    seen: DefaultDict[Coord, int] = defaultdict(int)
+
+    for line in get_lines(parsed_input):
+        for coord in line:
+            seen[coord] += 1
+
+    return len([value for value in seen.values() if value > 1])
+
+
+def parse_input(input_data: List[str], filter_diagonal: bool = True) -> List[LinePositions]:
+    lines: List[LinePositions] = []
+
+    for line in input_data:
+        x1, y1, x2, y2 = map(int, line_pattern.findall(line))
+        parsed_line = ((x1, y1), (x2, y2))
+
+        if filter_diagonal and is_diagonal(parsed_line):
+            continue
+
+        lines.append(parsed_line)
+
+    return lines
+
+
+@solution_timer(2021, 5, 1)
+def part_one(input_data: List[str]):
+    parsed_input = parse_input(input_data)
+    answer = count_intersections(parsed_input)
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 5, 1)
+
+    return answer
+
+
+@solution_timer(2021, 5, 2)
+def part_two(input_data: List[str]):
+    parsed_input = parse_input(input_data, filter_diagonal=False)
+    answer = count_intersections(parsed_input)
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 5, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2021, 5)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2021/test_day_05_2021.py
@@ -0,0 +1,50 @@
+import pytest
+
+from adventofcode.year_2021.day_05_2021 import part_two, part_one, parse_input, get_line, count_intersections
+
+test_input = [
+    '0,9 -> 5,9',
+    '8,0 -> 0,8',
+    '9,4 -> 3,4',
+    '2,2 -> 2,1',
+    '7,0 -> 7,4',
+    '6,4 -> 2,0',
+    '0,9 -> 2,9',
+    '3,4 -> 1,4',
+    '0,0 -> 8,8',
+    '5,5 -> 8,2',
+]
+
+
+def test_parse_input():
+    parsed_input_all = parse_input(test_input, False)
+    assert len(parsed_input_all) == 10
+
+    parsed_input_no_diagonals = parse_input(test_input, True)
+    assert len(parsed_input_no_diagonals) == 6
+
+
+@pytest.mark.parametrize(['positions', 'expected'], [
+    (((0, 9), (5, 9)), {(0, 9), (1, 9), (2, 9), (3, 9), (4, 9), (5, 9)})
+])
+def test_get_line(positions, expected):
+    assert get_line(positions) == expected
+
+
+@pytest.mark.parametrize(['exclude_diagonals', 'expected'], [
+    (True, 5),
+    (False, 12),
+])
+def test_count_intersections(exclude_diagonals, expected):
+    parsed_input = parse_input(test_input, exclude_diagonals)
+    assert count_intersections(parsed_input) == expected
+
+
+def test_part_one():
+    answer = part_one(test_input)
+    assert answer == 5
+
+
+def test_part_two():
+    answer = part_two(test_input)
+    assert answer == 12
