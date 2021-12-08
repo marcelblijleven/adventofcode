e2a8a56ba1e9f6316858cf36ed2c79d5c914865d
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-08 18:40:04 +0100

    feat: add math helpers

---
+++ src/adventofcode/util/math_helpers.py
@@ -0,0 +1,37 @@
+from typing import List
+
+import math
+
+
+def mean(target_list: List[int], floor: bool = True) -> int:
+    """
+    Gets the mean value of the list, rounded down if floor is True, else rounded up
+    """
+    if floor:
+        return mean_floor(target_list)
+
+    return mean_ceil(target_list)
+
+
+def mean_floor(target_list: List[int]) -> int:
+    """
+    Gets the rounded down mean of the list
+    """
+    return sum(target_list) // len(target_list)
+
+
+def mean_ceil(target_list: List[int]) -> int:
+    """
+    Gets the rounded up mean of the list
+    """
+    return math.ceil(sum(target_list) / len(target_list))
+
+
+def gaussian_sum(number: int) -> int:
+    """
+    Gets the sum of all numbers up to the provided number.
+    E.g. gaussian_sum(5) == sum([1, 2, 3, 4, 5])
+    :param number:
+    :return:
+    """
+    return number * (1 + number) // 2
