b44285a97a16bffa3c58181fe1f7de501ff6b3cc
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-09 21:36:46 +0100

    solution: add 2020 day 15

---
+++ src/adventofcode/year_2020/day_15_2020.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def parse_input(input_data: List[str]) -> List[int]:
+    return list(map(int, input_data[0].split(',')))
+
+
+def solve(input_data: List[int], rounds: int) -> int:
+    seen = {value: index + 1 for index, value in enumerate(input_data[:-1])}
+    number = input_data[-1]
+
+    for play_round in range(len(input_data), rounds):
+        seen[number], number = play_round, 0 if number not in seen else play_round - seen[number]
+    return number
+
+
+@solution_timer(2020, 15, 1)
+def part_one(input_data: List[str]):
+    answer = solve(parse_input(input_data), 2020)
+
+    if not answer:
+        raise SolutionNotFoundException(2020, 15, 1)
+
+    return answer
+
+
+@solution_timer(2020, 15, 2)
+def part_two(input_data: List[str]):
+    answer = solve(parse_input(input_data), 30000000)
+
+    if not answer:
+        raise SolutionNotFoundException(2020, 15, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2020, 15)
+    part_one(data)
+    part_two(data)
---
+++ tests/year_2020/test_day_15_2020.py
@@ -0,0 +1,36 @@
+import os
+
+import pytest
+
+from adventofcode.year_2020.day_15_2020 import part_two, part_one, parse_input, solve
+
+test_input = [
+    '0,3,6',
+]
+
+
+def test_parse_input():
+    assert parse_input(test_input) == [0, 3, 6]
+
+
+@pytest.mark.parametrize(['starting_list', 'rounds', 'expected'], [
+    ([0, 3, 6], 10, 0),
+    ([0, 3, 6], 2020, 436),
+    ([1, 3, 2], 2020, 1),
+    ([2, 1, 3], 2020, 10),
+    ([1, 2, 3], 2020, 27),
+    ([2, 3, 1], 2020, 78),
+    ([3, 2, 1], 2020, 438),
+    ([3, 1, 2], 2020, 1836),
+])
+def test_solve_part_one(starting_list, rounds, expected):
+    assert solve(starting_list, rounds) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 436
+
+
+@pytest.mark.skipif(os.getenv('CI', 0) == 'true', reason='slow test')
+def test_part_two():
+    assert part_two(test_input) == 175594
