6c265cdadb0255fdf82b0a0d183ead2a5072f354
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-14 17:37:26 +0100

    solution: add 2021 day 14

---
+++ tests/year_2021/test_day_14_2021.py
@@ -0,0 +1,12 @@
+from adventofcode.year_2021.day_14_2021 import part_two, part_one
+
+
+test_input = []
+
+
+def test_part_one():
+    assert part_one(test_input) == 'x'
+
+
+def test_part_two():
+    assert part_two(test_input) == 'x'
