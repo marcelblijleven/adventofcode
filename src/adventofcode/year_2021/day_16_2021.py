58442e69abfefd202d388558b14c3fe70fc057b9
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-17 15:16:47 +0100

    solution: add 2021 day 16 part 2

--- src/adventofcode/year_2021/day_16_2021.py
+++ src/adventofcode/year_2021/day_16_2021.py
@@ -1,6 +1,8 @@
 import io
 from typing import List
 
+import math
+
 from adventofcode.util.exceptions import SolutionNotFoundException
 from adventofcode.util.helpers import solution_timer
 from adventofcode.util.input_helpers import get_input_for_day
@@ -79,7 +81,7 @@ def input_to_binary_string(input_data: str) -> str:
     """
     Translates the provided (hex) input data to a string of 0 and 1
     """
-    return input_data.translate(str.maketrans(translation_dict))
+    return input_data.translate(str.maketrans(translation_dict))  # type: ignore
 
 
 def input_to_binary_string_faster(input_data: str) -> str:
@@ -93,21 +95,73 @@ def input_to_binary_string_faster(input_data: str) -> str:
     four, but not a multiple of four, the total length becomes total length plus the remainder of total length % 4,
     making it a multiple of four.
 
-    NOTE: This function is faster than using the translate table in function input_to_binary_string
-    (0.02 vs 0.17ms with puzzle input), but results in incorrect value for 620080001611562C8802118E34
+    NOTE:
+    if the remainder >= 2, it should zfill with the difference between 4 and the remainder,
+    if it is less than 2, it should zfill with the remainder itself.
+
+    Discovered this with 620080001611562C8802118E34 and 38006F45291200
     """
     value = bin(int(input_data, 16))[2:]
 
     if (len_value := len(value)) < 4:
         return value.zfill(4)
 
-    if (remainder := len_value % 4) != 0:
+    remainder = len_value % 4
+
+    if remainder >= 2:
+        return value.zfill(len_value + (4 - remainder))
+    else:
         return value.zfill(len_value + remainder)
 
-    return value
+
+def _process_type_ids_0123(type_id: int, values: list[int]) -> int:
+    """
+    Processes values with type id 0, 1, 2 or 3
+    """
+    if type_id == 0:
+        # sum
+        return sum(values)
+    elif type_id == 1:  # product
+        # product
+        return math.prod(values)
+    elif type_id == 2:
+        # min
+        return min(values)
+    elif type_id == 3:
+        # max
+        return max(values)
 
 
-def read_outer_packet(buffer: io.StringIO) -> int:
+def _process_type_ids_567(type_id: int, values: list[int]) -> int:
+    """
+    Processes values with type id 5, 6 or 7
+    """
+    if len(values) != 2:
+        raise ValueError(f'values should have length of 2, got: {values}')
+
+    if type_id == 5:
+        # greater than
+        return int(values[0] > values[1])
+    elif type_id == 6:
+        # less than
+        return int(values[0] < values[1])
+    else:
+        return int(values[0] == values[1])
+
+
+def process_values(type_id: int, values: list[int]) -> int:
+    """
+    Retrieves the packet value by applying the type id of the parent operator packet to the list of values
+    """
+    if type_id in [0, 1, 2, 3]:
+        return _process_type_ids_0123(type_id, values)
+    elif type_id in [5, 6, 7]:
+        return _process_type_ids_567(type_id, values)
+    else:
+        raise ValueError(f'unexpected type id received: {type_id}')
+
+
+def read_outer_packet(buffer: io.StringIO) -> tuple[list[int], int]:
     """
     Reads the most outer packet and returns the sum of all versions in the packet and any sub-packet
     """
@@ -116,27 +170,32 @@ def read_outer_packet(buffer: io.StringIO) -> int:
     type_id = int(buffer.read(3), 2)
 
     if type_id == 4:
-        return version
+        buffer.seek(buffer.tell() - 6)
+        value = read_type_four_packet(buffer)
+        return [version], value
     else:
         versions.append(version)
         length_type = int(buffer.read(1), 2)
 
         if length_type == 0:
             total_length = int(buffer.read(15), 2)
-            versions += read_packets_until_length(buffer, total_length)
+            packet_versions, packet_value = read_packets_until_length(buffer, total_length, type_id)
+            versions += packet_versions
         else:
             number_of_packets = int(buffer.read(11), 2)
-            versions += read_packets_until_all_read(buffer, number_of_packets)
+            packet_versions, packet_value = read_packets_until_all_read(buffer, number_of_packets, type_id)
+            versions += packet_versions
 
     read_garbage_bits(buffer)
-    return sum(versions)
+    return versions, packet_value
 
 
-def read_packets_until_length(buffer: io.StringIO, total_length: int) -> list[int]:
+def read_packets_until_length(buffer: io.StringIO, total_length: int, parent_type_id: int) -> tuple[list[int], int]:
     """
     Reads packets from the buffer until the total amount of bits read equals the total length parameter
     """
     versions: list[int] = []
+    values: list[int] = []
     start_position = buffer.tell()
 
     while buffer.tell() - start_position < total_length:
@@ -144,21 +203,27 @@ def read_packets_until_length(buffer: io.StringIO, total_length: int) -> list[in
 
         if type_id == 4:
             versions.append(version)
-            value = read_type_four_packet(buffer)
+            values.append(read_type_four_packet(buffer))
         else:
-            versions += read_operator_packet(buffer)
+            packet_versions, packet_value = read_operator_packet(buffer, type_id)
+            versions += packet_versions
+            values.append(packet_value)
 
         if buffer.tell() - start_position > total_length:
             raise ValueError('read past total length of packet')
 
-    return versions
+    value = process_values(parent_type_id, values)
+    return versions, value
 
 
-def read_packets_until_all_read(buffer: io.StringIO, number_of_packets: int) -> list[int]:
+def read_packets_until_all_read(
+    buffer: io.StringIO, number_of_packets: int, parent_type_id: int
+) -> tuple[list[int], int]:
     """
     Reads packets from the buffer until the number of packets provided has been read
     """
     versions: list[int] = []
+    values: list[int] = []
     packets_read = 0
 
     while packets_read < number_of_packets:
@@ -167,12 +232,16 @@ def read_packets_until_all_read(buffer: io.StringIO, number_of_packets: int) -> 
         if type_id == 4:
             versions.append(version)
             value = read_type_four_packet(buffer)
+            values.append(value)
         else:
-            versions += read_operator_packet(buffer)
+            packet_versions, packet_value = read_operator_packet(buffer, type_id)
+            versions += packet_versions
+            values.append(packet_value)
 
         packets_read += 1
 
-    return versions
+    value = process_values(parent_type_id, values)
+    return versions, value
 
 
 def read_type_four_packet(buffer: io.StringIO) -> int:
@@ -193,7 +262,7 @@ def read_type_four_packet(buffer: io.StringIO) -> int:
     return int(value, 2)
 
 
-def read_operator_packet(buffer: io.StringIO) -> list[int]:
+def read_operator_packet(buffer: io.StringIO, parent_type_id: int) -> tuple[list[int], int]:
     """
     Determines the length type id for the packet and calls the correct operator packet
     reader.
@@ -201,19 +270,22 @@ def read_operator_packet(buffer: io.StringIO) -> list[int]:
     """
     version = int(buffer.read(3), 2)
     type_id = int(buffer.read(3), 2)
+    assert type_id == parent_type_id
     length_type_id = int(buffer.read(1), 2)
     versions = [version]
 
     if length_type_id == 0:
         total_length = int(buffer.read(15), 2)
-        versions += read_packets_until_length(buffer, total_length)
+        packet_versions, packet_value = read_packets_until_length(buffer, total_length, parent_type_id)
     elif length_type_id == 1:
         total_packets = int(buffer.read(11), 2)
-        versions += read_packets_until_all_read(buffer, total_packets)
+        packet_versions, packet_value = read_packets_until_all_read(buffer, total_packets, parent_type_id)
     else:
         raise ValueError(f'unexpected length type "{length_type_id}" received')
 
-    return versions
+    versions += packet_versions
+
+    return versions, packet_value
 
 
 def read_garbage_bits(buffer: io.StringIO) -> None:
@@ -229,9 +301,10 @@ def read_garbage_bits(buffer: io.StringIO) -> None:
 def part_one(input_data: List[str]):
     binary_string = input_to_binary_string(input_data[0])
     buffer = io.StringIO(binary_string)
-    answer = read_outer_packet(buffer)
+    versions, _ = read_outer_packet(buffer)
+    answer = sum(versions)
 
-    if not answer:
+    if answer is None:
         raise SolutionNotFoundException(2021, 16, 1)
 
     return answer
@@ -239,9 +312,12 @@ def part_one(input_data: List[str]):
 
 @solution_timer(2021, 16, 2)
 def part_two(input_data: List[str]):
-    answer = ...
+    binary_string = input_to_binary_string(input_data[0])
+    buffer = io.StringIO(binary_string)
+    _, value = read_outer_packet(buffer)
+    answer = value
 
-    if not answer:
+    if answer is None:
         raise SolutionNotFoundException(2021, 16, 2)
 
     return answer
--- tests/adventofcode/year_2021/test_day_16_2021.py
+++ tests/adventofcode/year_2021/test_day_16_2021.py
@@ -4,7 +4,8 @@ import pytest
 import pytest_mock
 
 from adventofcode.year_2021.day_16_2021 import part_one, input_to_binary_string, \
-    read_type_four_packet, input_to_binary_string_faster, read_operator_packet, read_outer_packet
+    read_type_four_packet, input_to_binary_string_faster, read_operator_packet, read_outer_packet, part_two, \
+    peek_version_type_id_and_length_type, peek_version_and_type_id
 
 
 @pytest.mark.parametrize(['hex_string', 'expected'], [
@@ -26,7 +27,11 @@ from adventofcode.year_2021.day_16_2021 import part_one, input_to_binary_string,
     ('F', '1111'),
     ('D2FE28', '110100101111111000101000'),
     ('EE00D40C823060', '11101110000000001101010000001100100000100011000001100000'),
-    ('38006F45291200', '00111000000000000110111101000101001010010001001000000000')
+    ('38006F45291200', '00111000000000000110111101000101001010010001001000000000'),
+    (
+        '620080001611562C8802118E34',
+        '01100010000000001000000000000000000101100001000101010110001011001000100000000010000100011000111000110100'
+    ),
 ])
 def test_input_to_binary_string(hex_string, expected):
     assert input_to_binary_string(hex_string) == expected
@@ -51,10 +56,54 @@ def test_input_to_binary_string(hex_string, expected):
     ('F', '1111'),
     ('D2FE28', '110100101111111000101000'),
     ('EE00D40C823060', '11101110000000001101010000001100100000100011000001100000'),
-    ('38006F45291200', '00111000000000000110111101000101001010010001001000000000')
+    ('38006F45291200', '00111000000000000110111101000101001010010001001000000000'),
+    (
+        '620080001611562C8802118E34',
+        '01100010000000001000000000000000000101100001000101010110001011001000100000000010000100011000111000110100'
+    ),
 ])
-def test_input_to_binary_string_two(hex_string, expected):
+def test_input_to_binary_string_faster(hex_string, expected):
     assert input_to_binary_string_faster(hex_string) == expected
+
+
+
+def test_peek_version_and_type_id():
+    # version
+    bin_string = ''
+    with pytest.raises(ValueError) as wrapped_e:
+        peek_version_and_type_id(io.StringIO(bin_string))
+
+    assert str(wrapped_e.value) == 'could not read version from buffer'
+
+    # type id
+    bin_string = '010'
+    with pytest.raises(ValueError) as wrapped_e:
+        peek_version_and_type_id(io.StringIO(bin_string))
+
+    assert str(wrapped_e.value) == 'could not read type id from buffer'
+
+
+def test_peek_version_type_id_and_length_type():
+    # version
+    bin_string = ''
+    with pytest.raises(ValueError) as wrapped_e:
+        peek_version_type_id_and_length_type(io.StringIO(bin_string))
+
+    assert str(wrapped_e.value) == 'could not read version from buffer'
+
+    # type id
+    bin_string = '010'
+    with pytest.raises(ValueError) as wrapped_e:
+        peek_version_type_id_and_length_type(io.StringIO(bin_string))
+
+    assert str(wrapped_e.value) == 'could not read type id from buffer'
+
+    # length
+    bin_string = '010101'
+    with pytest.raises(ValueError) as wrapped_e:
+        peek_version_type_id_and_length_type(io.StringIO(bin_string))
+
+    assert str(wrapped_e.value) == 'could not read length type id from buffer'
 
 
 def test_read_type_four_packet():
@@ -69,8 +118,9 @@ def test_read_operator_packet(mocker: pytest_mock.MockerFixture):
     type_four = mocker.patch('adventofcode.year_2021.day_16_2021.read_type_four_packet')
     type_four.side_effect = read_type_four_packet
 
-    read_operator_packet(buffer)
+    _, value = read_operator_packet(buffer, 6)
     assert type_four.call_count == 2
+    assert value == 1
 
 
 @pytest.mark.parametrize(['transmission', 'expected'], [
@@ -83,7 +133,8 @@ def test_read_outer_packet(transmission, expected):
     hex_string = transmission
     bin_string = input_to_binary_string(hex_string)
     buffer = io.StringIO(bin_string)
-    assert read_outer_packet(buffer) == expected
+    versions, value = read_outer_packet(buffer)
+    assert sum(versions) == expected
 
 
 @pytest.mark.parametrize(['transmission', 'expected'], [
@@ -95,5 +146,16 @@ def test_read_outer_packet(transmission, expected):
 def test_part_one(transmission, expected):
     assert part_one([transmission]) == expected
 
-# def test_part_two():
-#     assert part_two(test_input) == 'x'
+
+@pytest.mark.parametrize(['transmission', 'expected'], [
+    ('C200B40A82', 3),
+    ('04005AC33890', 54),
+    ('880086C3E88112', 7),
+    ('CE00C43D881120', 9),
+    ('D8005AC2A8F0', 1),
+    ('F600BC2D8F', 0),
+    ('9C005AC2F8F0', 0),
+    ('9C0141080250320F1802104A08', 1),
+])
+def test_part_two(transmission, expected):
+    assert part_two([transmission]) == expected
