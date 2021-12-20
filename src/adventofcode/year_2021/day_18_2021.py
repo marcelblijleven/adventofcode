ef90ce98d687769feab1dff5300b60ab0857d289
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-20 08:42:39 +0100

    solution: add 2021 day 18

---
+++ src/adventofcode/year_2021/day_18_2021.py
@@ -0,0 +1,175 @@
+import re
+from itertools import permutations
+from typing import List, Union
+
+import math
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer
+from adventofcode.util.input_helpers import get_input_for_day
+
+pair_pattern = re.compile(r'(\[\d+,\d+])')
+single_number_pattern = re.compile(r'(\d+)')
+
+
+def get_pair_level(pair_start: int, snailfish_number: str) -> int:
+    sub_string = snailfish_number[:pair_start]
+    opening_brackets = sub_string.count('[')
+    closing_brackets = sub_string.count(']')
+    return opening_brackets - closing_brackets
+
+
+def get_pair_numbers(pair: str) -> tuple[int, int]:
+    if not pair.startswith('[') or not pair.endswith(']'):
+        raise ValueError(f'invalid pair: {pair}')
+
+    pair = pair.lstrip('[').rstrip(']')
+    left, right = pair.split(',')
+    return int(left), int(right)
+
+
+def apply_explode_update_left(number: int, sub_string: str) -> str:
+    matches = list(single_number_pattern.finditer(sub_string))
+
+    if len(matches) == 0:
+        return sub_string
+
+    last_match = matches[-1]
+    value = last_match.group()
+    span = last_match.span()
+    return f'{sub_string[:span[0]]}{int(value) + number}{sub_string[span[1]:]}'
+
+
+def apply_explode_update_right(number: int, sub_string: str) -> str:
+    matches = list(single_number_pattern.finditer(sub_string))
+
+    if len(matches) == 0:
+        return sub_string
+
+    first_match = matches[0]
+    value = first_match.group()
+    span = first_match.span()
+    return f'{sub_string[:span[0]]}{int(value) + number}{sub_string[span[1]:]}'
+
+
+def explode(snailfish_number: str) -> tuple[str, bool]:
+    for match in pair_pattern.finditer(snailfish_number):
+        if get_pair_level(match.start(), snailfish_number) == 4:
+            left, right = get_pair_numbers(match.group())
+            span = match.span()
+            left_of_pair = snailfish_number[:span[0]]
+            right_of_pair = snailfish_number[span[1]:]
+            left_sub_string = apply_explode_update_left(left, left_of_pair)
+            right_sub_string = apply_explode_update_right(right, right_of_pair)
+            new_number = f'{left_sub_string}0{right_sub_string}'
+            return new_number, True
+
+    return snailfish_number, False
+
+
+def split_number(number: int) -> tuple[int, int]:
+    half = number / 2
+    return math.floor(half), math.ceil(half)
+
+
+def split(snailfish_number: str) -> tuple[str, bool]:
+    pattern = re.compile(r'(\d+)')
+
+    for match in pattern.finditer(snailfish_number):
+        if (number := int(match.group())) > 9:
+            left, right = split_number(number)
+            span = match.span()
+            left_of_pair = snailfish_number[:span[0]]
+            right_of_pair = snailfish_number[span[1]:]
+            return f'{left_of_pair}[{left},{right}]{right_of_pair}', True
+
+    return snailfish_number, False
+
+
+def addition(snailfish_number: str, to_add: str) -> str:
+    return f'[{snailfish_number},{to_add}]'
+
+
+def reduce_snailfish_number(snailfish_number: str) -> str:
+    while True:
+        snailfish_number, exploded = explode(snailfish_number)
+
+        if exploded:
+            continue
+
+        snailfish_number, was_split = split(snailfish_number)
+
+        if not exploded and not was_split:
+            return snailfish_number
+
+
+def parse_snailfish_number(snailfish_number: str, next_value: str) -> str:
+    snailfish_number = addition(snailfish_number, next_value)
+    return reduce_snailfish_number(snailfish_number)
+
+
+def parse_snailfish_numbers(snailfish_numbers: Union[list[str], tuple[str, ...]]) -> str:
+    result = snailfish_numbers[0]
+
+    for number in snailfish_numbers[1:]:
+        result = parse_snailfish_number(result, number)
+
+    return result
+
+
+def calculate_magnitude(snailfish_number: str) -> int:
+    pairs = list(pair_pattern.finditer(snailfish_number))
+
+    if not pairs:
+        return int(snailfish_number)
+
+    for pair in pairs[::-1]:
+        left, right = get_pair_numbers(pair.group())
+        pair_value = left * 3 + right * 2
+        span = pair.span()
+        snailfish_number = f'{snailfish_number[:span[0]]}{pair_value}{snailfish_number[span[1]:]}'
+        return calculate_magnitude(snailfish_number)
+
+    raise ValueError(f'cannot parse snailfish number: {snailfish_number}')
+
+
+def solve_homework(input_data: List[str]) -> int:
+    result = parse_snailfish_numbers(input_data)
+    return calculate_magnitude(result)
+
+
+def solve_homework_part_two(input_data: List[str]) -> int:
+    max_magnitude = 0
+
+    for perm in permutations(input_data, 2):
+        result = parse_snailfish_numbers(perm)
+        magnitude = calculate_magnitude(result)
+        max_magnitude = max(magnitude, max_magnitude)
+
+    return max_magnitude
+
+
+@solution_timer(2021, 18, 1)
+def part_one(input_data: List[str]):
+    answer = solve_homework(input_data)
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 18, 1)
+
+    return answer
+
+
+@solution_timer(2021, 18, 2)
+def part_two(input_data: List[str]):
+    answer = solve_homework_part_two(input_data)
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 18, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2021, 18)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2021/test_day_18_2021.py
@@ -0,0 +1,98 @@
+import pytest
+
+from adventofcode.year_2021.day_18_2021 import part_two, part_one, explode, addition, parse_snailfish_number, \
+    reduce_snailfish_number, parse_snailfish_numbers, calculate_magnitude, get_pair_numbers
+
+test_input = [
+    '[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]',
+    '[[[5,[2,8]],4],[5,[[9,9],0]]]',
+    '[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]',
+    '[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]',
+    '[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]',
+    '[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]',
+    '[[[[5,4],[7,7]],8],[[8,3],8]]',
+    '[[9,3],[[9,9],[6,[4,9]]]]',
+    '[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]',
+    '[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]',
+]
+
+
+@pytest.mark.parametrize(['number', 'expected'], [
+    ('[[[[[9,8],1],2],3],4]', '[[[[0,9],2],3],4]'),
+    ('[7,[6,[5,[4,[3,2]]]]]', '[7,[6,[5,[7,0]]]]'),
+    ('[[6,[5,[4,[3,2]]]],1]', '[[6,[5,[7,0]]],3]'),
+    ('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'),
+    ('[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]', '[[3,[2,[8,0]]],[9,[5,[7,0]]]]'),
+])
+def test_explode(number, expected):
+    assert explode(number) == (expected, True)
+
+
+def test_get_pair_numbers():
+    assert get_pair_numbers('[1,2]') == (1, 2)
+    assert get_pair_numbers('[30,40]') == (30, 40)
+    assert get_pair_numbers('[500,6]') == (500, 6)
+
+    with pytest.raises(ValueError) as wrapped_e:
+        get_pair_numbers('1,2')
+
+    assert str(wrapped_e.value) == 'invalid pair: 1,2'
+
+
+def test_addition():
+    snailfish_number = '[[[[4,3],4],4],[7,[[8,4],9]]]'
+    to_add = '[1,1]'
+    assert addition(snailfish_number, to_add) == '[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'
+
+
+@pytest.mark.parametrize(['number', 'expected'], [
+    ('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]', '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]')
+])
+def test_reduce_snailfish_number(number, expected):
+    assert reduce_snailfish_number(number) == expected
+
+
+def test_parse_snailfish_number():
+    snailfish_number = '[[[[4,3],4],4],[7,[[8,4],9]]]'
+    assert parse_snailfish_number(snailfish_number, '[1,1]') == '[[[[0,7],4],[[7,8],[6,0]]],[8,1]]'
+
+
+@pytest.mark.parametrize(['numbers', 'expected'], [
+    ('[1,1]\n[2,2]\n[3,3]\n[4,4]', '[[[[1,1],[2,2]],[3,3]],[4,4]]'),
+    ('[1,1]\n[2,2]\n[3,3]\n[4,4]\n[5,5]', '[[[[3,0],[5,3]],[4,4]],[5,5]]'),
+    ('[1,1]\n[2,2]\n[3,3]\n[4,4]\n[5,5]\n[6,6]', '[[[[5,0],[7,4]],[5,5]],[6,6]]'),
+    ('\n'.join([
+        '[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]',
+        '[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]',
+        '[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]',
+        '[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]',
+        '[7,[5,[[3,8],[1,4]]]]',
+        '[[2,[2,2]],[8,[8,1]]]',
+        '[2,9]',
+        '[1,[[[9,3],9],[[9,0],[0,7]]]]',
+        '[[[5,[7,4]],7],1]',
+        '[[[[4,2],2],6],[8,7]]',
+    ]), '[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]'),
+])
+def test_parse_snailfish_numbers(numbers, expected):
+    assert parse_snailfish_numbers(numbers.split('\n')) == expected
+
+
+@pytest.mark.parametrize(['number', 'expected'], [
+    ('[[1,2],[[3,4],5]]', 143),
+    ('[[[[0,7],4],[[7,8],[6,0]]],[8,1]]', 1384),
+    ('[[[[1,1],[2,2]],[3,3]],[4,4]]', 445),
+    ('[[[[3,0],[5,3]],[4,4]],[5,5]]', 791),
+    ('[[[[5,0],[7,4]],[5,5]],[6,6]]', 1137),
+    ('[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]', 3488),
+])
+def test_calculate_magnitude(number, expected):
+    assert calculate_magnitude(number) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 4140
+
+
+def test_part_two():
+    assert part_two(test_input) == 3993
