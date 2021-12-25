c62ad594c33018e09819bda8c6a49d6518542952
Marcel Blijleven <marcelblijleven@gmail.com>
2021-12-25 20:15:09 +0100

    solution: add 2021 day 20, 21 and 22

---
+++ src/adventofcode/year_2021/day_21_2021.py
@@ -0,0 +1,146 @@
+from __future__ import annotations
+
+from typing import List
+
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.helpers import solution_timer, memoize
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def parse_input(input_data: list[str]) -> tuple[int, int]:
+    player_one = int(input_data[0][-1])
+    player_two = int(input_data[1][-1])
+    return player_one, player_two
+
+
+def get_new_position(current_position: int, moves: int) -> int:
+    max_position = 10
+    if (next_position := (current_position + moves) % max_position) != 0:
+        return next_position
+
+    return max_position
+
+
+class DeterministicDie:
+    def __init__(self):
+        self.rolls = 0
+        self._next_roll = 1
+        self._last_roll = 0
+
+    def roll(self) -> int:
+        roll = self._next_roll
+
+        if self._next_roll + 1 > 100:
+            self._next_roll = 1
+        else:
+            self._next_roll += 1
+
+        self._last_roll = roll
+        self.rolls += 1
+        return roll
+
+
+class Player:
+    def __init__(self, number: int, starting_position: int, max_score: int = 1000):
+        self.number = number
+        self._position = starting_position
+        self._score = 0
+        self._max_score = max_score
+
+    @property
+    def score(self) -> int:
+        return self._score
+
+    @property
+    def has_won(self) -> bool:
+        return self.score >= self._max_score
+
+    def add_to_score(self, addition: int):
+        self._score += addition
+
+    def move(self, moves: int):
+        self._position = get_new_position(self._position, moves)
+        self.add_to_score(self._position)
+
+    def roll_die(self, die: DeterministicDie) -> bool:
+        moves = []
+
+        for _ in range(3):
+            moves.append(die.roll())
+
+        self.move(sum(moves))
+        # print(
+        #   f'Player {self.number} rolls {"+".join(map(str, moves))} and moves to space {self._position} ' +
+        #   'for a total score of {self.score}'
+        # )
+        return self.has_won
+
+
+def game(player_one: Player, player_two: Player) -> int:
+    players = [player_one, player_two]
+    die = DeterministicDie()
+    rounds = 0
+
+    while True:
+        player = players[rounds % 2]
+        if player.roll_die(die):
+            break
+
+        rounds += 1
+
+    rolls = die.rolls
+    losing_score = [p for p in players if not p.has_won][0].score
+    return rolls * losing_score
+
+
+def apply_wins(wins: tuple[int, int], universe_wins: tuple[int, int]) -> tuple[int, int]:
+    return wins[0] + universe_wins[1], wins[1] + universe_wins[0]
+
+
+@memoize
+def quantum_game(player_one_pos, player_two_pos, player_one_score, player_two_score):
+    if player_one_score >= 21:
+        return 1, 0
+    elif player_two_score >= 21:
+        return 0, 1
+
+    wins = (0, 0)
+
+    for r1 in [1, 2, 3]:
+        for r2 in [1, 2, 3]:
+            for r3 in [1, 2, 3]:
+                next_pos = get_new_position(player_one_pos, r1 + r2 + r3)
+                score = player_one_score + next_pos
+                universe_wins = quantum_game(player_two_pos, next_pos, player_two_score, score)
+                wins = apply_wins(wins, universe_wins)
+
+    return wins
+
+
+@solution_timer(2021, 21, 1)
+def part_one(input_data: List[str]):
+    positions = parse_input(input_data)
+    answer = game(Player(1, positions[0]), Player(2, positions[1]))
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 21, 1)
+
+    return answer
+
+
+@solution_timer(2021, 21, 2)
+def part_two(input_data: List[str]):
+    positions = parse_input(input_data)
+    seen: dict[tuple[Player, Player], tuple[int, int]]
+    answer = max(quantum_game(positions[0], positions[1], 0, 0))
+
+    if not answer:
+        raise SolutionNotFoundException(2021, 21, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2021, 21)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2021/test_day_21_2021.py
@@ -0,0 +1,35 @@
+import pytest
+from adventofcode.year_2021.day_21_2021 import part_two, part_one, parse_input, get_new_position
+
+test_input = [
+    'Player 1 starting position: 4',
+    'Player 2 starting position: 8',
+]
+
+
+def test_parse_input():
+    assert parse_input(test_input) == (4, 8)
+
+
+@pytest.mark.parametrize(['current', 'moves', 'expected'], [
+    (1, 5, 6),
+    (2, 5, 7),
+    (3, 5, 8),
+    (4, 5, 9),
+    (5, 5, 10),
+    (6, 5, 1),
+    (7, 5, 2),
+    (8, 5, 3),
+    (9, 5, 4),
+    (10, 5, 5),
+])
+def test_get_new_position(current, moves, expected):
+    assert get_new_position(current, moves) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 739785
+
+
+def test_part_two():
+    assert part_two(test_input) == 444356092776315
