cefbe2290aa2e382162f86a6b0c3302677d7ec68
Marcel Blijleven <marcelblijleven@gmail.com>
2022-12-01 10:43:17 +0100

    solution: add day 1 of 2022

---
+++ src/adventofcode/year_2022/day_01_2022.py
@@ -0,0 +1,61 @@
+from adventofcode.registry.decorators import register_solution
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def find_most_calories(input_data: list[str]) -> int:
+    """Find the most calories a single Elf carries"""
+    calories = 0
+    max_calories = 0
+
+    for row in input_data + ['']:
+        if row == '':
+            max_calories = calories if calories > max_calories else max_calories
+            calories = 0
+            continue
+
+        calories += int(row)
+
+    return max_calories
+
+
+def find_top_three_most_calories(input_data: list[str]) -> int:
+    """Find the sum of calories of the top 3 Elves"""
+    calories = 0
+    chunks = []
+
+    for row in input_data + ['']:
+        if row == '':
+            chunks.append(calories)
+            calories = 0
+            continue
+        calories += int(row)
+
+    chunks.sort(reverse=True)
+    return sum(chunks[:3])
+
+
+@register_solution(2022, 1, 1)
+def part_one(input_data: list[str]):
+    answer = find_most_calories(input_data)
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 1, 1)
+
+    return answer
+
+
+@register_solution(2022, 1, 2)
+def part_two(input_data: list[str]):
+    answer = find_top_three_most_calories(input_data)
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 1, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2022, 1)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2022/test_day_01_2022.py
@@ -0,0 +1,30 @@
+from adventofcode.year_2022.day_01_2022 import part_two, part_one, find_most_calories
+
+test_input = [
+    '1000',
+    '2000',
+    '3000',
+    '',
+    '4000',
+    '',
+    '5000',
+    '6000',
+    '',
+    '7000',
+    '8000',
+    '9000',
+    '',
+    '10000',
+]
+
+
+def test_find_elf_with_most_calories():
+    assert find_most_calories(test_input) == 24000
+
+
+def test_part_one():
+    assert part_one(test_input) == 24000
+
+
+def test_part_two():
+    assert part_two(test_input) == 45000
