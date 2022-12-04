6b5bea40b78da2540e72b4171d751897be2bae31
Marcel Blijleven <marcelblijleven@gmail.com>
2022-12-04 22:29:17 +0100

    test: parametrize test

--- tests/adventofcode/year_2022/test_day_04_2022.py
+++ tests/adventofcode/year_2022/test_day_04_2022.py
@@ -1,3 +1,5 @@
+import pytest
+
 from adventofcode.year_2022.day_04_2022 import (
     part_two,
     part_one,
@@ -16,6 +18,24 @@ test_input = [
     "6-6,4-6",
     "2-6,4-8",
 ]
+
+
+@pytest.mark.parametrize(
+    ["row", "expected"],
+    zip(
+        test_input,
+        [
+            ((2, 4), (6, 8)),
+            ((2, 3), (4, 5)),
+            ((5, 7), (7, 9)),
+            ((2, 8), (3, 7)),
+            ((6, 6), (4, 6)),
+            ((2, 6), (4, 8)),
+        ],
+    ),
+)
+def test_get_pairs(row, expected):
+    assert get_pairs(row) == expected
 
 
 def test_does_contain():
