b04aa173e5e4390f0c961c1cf6e55ab8d8169b6c
Marcel Blijleven <marcelblijleven@gmail.com>
2022-12-06 20:06:04 +0100

    solution: add day 5 of 2022

---
+++ src/adventofcode/year_2022/day_05_2022.py
@@ -0,0 +1,157 @@
+import re
+import string
+
+from typing import DefaultDict, Callable, Generator
+from collections import defaultdict
+from adventofcode.util.exceptions import SolutionNotFoundException
+from adventofcode.registry.decorators import register_solution
+from adventofcode.util.input_helpers import get_input_for_day
+
+INSTRUCTIONS_PATTERN = re.compile(r"(\d+)")
+
+
+def get_number_of_crates(len_of_line: int) -> int:
+    """
+    Get number of crates.
+    The length of the provided line will be divided by 4 (each crate consists of 3
+    characters + a space) and rounded up to get the number of crates
+    """
+    return round(len_of_line / 4)
+
+
+def parse_crates(lines: list[str]) -> defaultdict[int, list[str]]:
+    """
+    Parse crate part of input, crate letters are at positions:
+    1, 5, 9, 13, 17, 21, 25, 29, 33
+    (1, 5, 9 for test input)
+
+    There are max 9 stacks of crates
+    """
+    stacks: DefaultDict[int, list[str]] = defaultdict(list)
+    number_of_crates = max(int(x) for x in INSTRUCTIONS_PATTERN.findall(lines[-1]))
+
+    indices = [1 + (idx * 4) for idx in range(0, number_of_crates)]
+
+    for row in lines:
+        for stack_no, idx in enumerate(indices):
+            if idx > len(row):
+                break
+            try:
+                string.ascii_uppercase.index(value := row[idx])
+                stacks[stack_no + 1].insert(0, value)
+            except (ValueError, IndexError):
+                pass
+
+    return stacks
+
+
+def parse_crates_option_two(lines: list[str]) -> defaultdict[int, list[str]]:
+    """
+    Parse crate part of input, crate letters are at positions:
+    1, 5, 9, 13, 17, 21, 25, 29, 33
+    (1, 5, 9 for test input)
+
+    There are max 9 stacks of crates
+
+    Different approach to filling the stacks, 0 to 2ms faster
+    """
+    stacks: DefaultDict[int, list[str]] = defaultdict(list)
+    number_of_crates = max(int(x) for x in INSTRUCTIONS_PATTERN.findall(lines[-1]))
+
+    indices = {1 + (idx * 4): idx + 1 for idx in range(number_of_crates)}
+
+    for row in lines:
+        for idx, value in enumerate(row):
+            if value.isalpha():
+                stacks[indices[idx]].insert(0, value)
+
+    return stacks
+
+
+def parse_instructions(lines: list[str]) -> Generator[tuple[int, ...], None, None]:
+    """Parse a line of instructions to a tuple of int"""
+    for line in lines:
+        if line:
+            yield tuple([int(x) for x in INSTRUCTIONS_PATTERN.findall(line)])
+
+
+def parse_input(input_data: list[str]) -> tuple[list[str], list[str]]:
+    """Splits the input in a section for crates and a section for instructions"""
+    crates: list[str] = []
+    instructions_start: int = 0
+
+    # read crates first
+    for idx, row in enumerate(input_data):
+        if not row:
+            instructions_start = idx
+            break
+
+        crates.append(row)
+
+    return crates, input_data[instructions_start:]
+
+
+def execute_instruction(crates: defaultdict[int, list[str]], instructions) -> None:
+    """Executes instructions for the CrateMover 9000"""
+    amount, source, destination = instructions
+
+    for i in range(amount):
+        try:
+            crates[destination].append(crates[source].pop())
+        except IndexError as exc:
+            raise ValueError(
+                f"tried to move a crate from an empty stack "
+                f"with instruction {instructions}"
+            ) from exc
+
+
+def execute_instruction_9001(crates: defaultdict[int, list[str]], instructions) -> None:
+    """Executes instructions for the CrateMover 9001"""
+    amount, source, destination = instructions
+
+    crates[destination] += crates[source][-1 * amount :]
+    crates[source] = crates[source][: -1 * amount]
+
+
+def move_crates(
+    input_data: list[str],
+    executor_func: Callable[[defaultdict[int, list[str]], tuple[int, ...]], None],
+) -> str:
+    crates_input, instructions_input = parse_input(input_data)
+    # crates = parse_crates(crates_input)
+    crates = parse_crates_option_two(crates_input)
+
+    for instruction in parse_instructions(instructions_input):
+        executor_func(crates, instruction)
+
+    output = ""
+    for key, value in sorted(crates.items()):
+        output += value[-1]
+
+    return output
+
+
+@register_solution(2022, 5, 1)
+def part_one(input_data: list[str]):
+    answer = move_crates(input_data, execute_instruction)
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 5, 1)
+
+    return answer
+
+
+@register_solution(2022, 5, 2)
+def part_two(input_data: list[str]):
+    answer = move_crates(input_data, execute_instruction_9001)
+
+    if not answer:
+        raise SolutionNotFoundException(2022, 5, 2)
+
+    return answer
+
+
+if __name__ == "__main__":
+    data = get_input_for_day(2022, 5)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2022/test_day_05_2022.py
@@ -0,0 +1,101 @@
+from adventofcode.year_2022.day_05_2022 import (
+    part_two,
+    part_one,
+    get_number_of_crates,
+    parse_crates,
+    parse_instructions,
+    execute_instruction,
+    move_crates,
+    execute_instruction_9001,
+    parse_crates_option_two,
+)
+
+test_input = [
+    "    [D]",
+    "[N] [C]",
+    "[Z] [M] [P]",
+    " 1   2   3 ",
+    "",
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2",
+]
+
+
+def test_get_number_of_crates():
+    assert get_number_of_crates(len("[N] [D] [M] [G] [Z] [F] [W] [S] [S]")) == 9
+    assert get_number_of_crates(len("[Z] [M] [P]")) == 3
+
+
+def test_parse_crates():
+    crate_input = test_input[:4]
+    crates = parse_crates(crate_input)
+    assert list(crates[1]) == ["Z", "N"]
+    assert list(crates[2]) == ["M", "C", "D"]
+    assert list(crates[3]) == ["P"]
+
+
+def test_parse_instructions():
+    instructions_input = test_input[4:]
+
+    for instruction in parse_instructions(instructions_input):
+        print(type(instruction), instruction)
+
+
+def test_execute_instructions():
+    crates = parse_crates(test_input[:4])
+    instruction = (1, 2, 1)
+
+    assert list(crates[1]) == ["Z", "N"]
+    assert list(crates[2]) == ["M", "C", "D"]
+    assert list(crates[3]) == ["P"]
+
+    execute_instruction(crates, instruction)
+    assert list(crates[1]) == ["Z", "N", "D"]
+    assert list(crates[2]) == ["M", "C"]
+    assert list(crates[3]) == ["P"]
+
+
+def test_execute_instructions_9001():
+    crates = parse_crates(test_input[:4])
+    instruction = (1, 2, 1)
+
+    assert list(crates[1]) == ["Z", "N"]
+    assert list(crates[2]) == ["M", "C", "D"]
+    assert list(crates[3]) == ["P"]
+
+    execute_instruction_9001(crates, instruction)
+    assert list(crates[1]) == ["Z", "N", "D"]
+    assert list(crates[2]) == ["M", "C"]
+    assert list(crates[3]) == ["P"]
+
+    instruction = (3, 1, 3)
+    execute_instruction_9001(crates, instruction)
+    assert list(crates[1]) == []
+    assert list(crates[2]) == ["M", "C"]
+    assert list(crates[3]) == ["P", "Z", "N", "D"]
+
+
+def test_parse_crates_option_two():
+    crates = parse_crates(test_input[:4])
+    crates_two = parse_crates_option_two(test_input[:4])
+
+    for key, value in crates.items():
+        assert crates_two[key] == value
+
+
+def test_move_crates():
+    assert "CMZ" == move_crates(test_input, execute_instruction)
+
+
+def test_move_crates_part_two():
+    assert "MCD" == move_crates(test_input, execute_instruction_9001)
+
+
+def test_part_one():
+    assert part_one(test_input) == "CMZ"
+
+
+def test_part_two():
+    assert part_two(test_input) == "MCD"
