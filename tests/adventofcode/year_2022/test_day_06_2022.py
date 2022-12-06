04a05f3875a31ba69735e0b8a3f4c3c7d745edc7
Marcel Blijleven <marcelblijleven@gmail.com>
2022-12-06 20:40:29 +0100

    solution: add day 6 of 2022

---
+++ tests/adventofcode/year_2022/test_day_06_2022.py
@@ -0,0 +1,44 @@
+import pytest
+from adventofcode.year_2022.day_06_2022 import (
+    part_two,
+    part_one,
+    find_marker,
+    find_message_marker,
+)
+
+test_input = ["mjqjpqmgbljsphdztnvjfqwrcgsmlb"]
+
+
+@pytest.mark.parametrize(
+    ["input_str", "expected"],
+    [
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 6),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
+    ],
+)
+def test_find_marker(input_str: str, expected: int):
+    assert find_marker(input_str) == expected
+
+
+@pytest.mark.parametrize(
+    ["input_str", "expected"],
+    [
+        ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+        ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+        ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+        ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+        ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+    ],
+)
+def test_find_message_marker(input_str: str, expected: int):
+    assert find_message_marker(input_str) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 7
+
+
+def test_part_two():
+    assert part_two(test_input) == 19
