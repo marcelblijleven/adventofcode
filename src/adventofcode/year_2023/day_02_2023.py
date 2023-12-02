ad8d2b0b07a37c80bf063acc52f621892e8a01c0
Marcel Blijleven <marcelblijleven@gmail.com>
2023-12-02 21:58:02 +0100

    feat: add 2023 day 2 part 1

---
+++ src/adventofcode/year_2023/day_02_2023.py
@@ -0,0 +1,79 @@
+import re
+
+from adventofcode.util.exceptions import SolutionNotFoundError
+from adventofcode.registry.decorators import register_solution
+from adventofcode.util.input_helpers import get_input_for_day
+
+game_pattern = re.compile("(?:(?P<amount>\d+)\s|(?P<color>red|blue|green))")
+
+
+def parse_game(line) -> list[dict[str, int]]:
+    prefix, game = line.split(": ")
+    game_values = []
+    rounds = game.split("; ")
+
+    for round in rounds:
+        pairs = round.split(", ")
+
+        round_values = {}
+        for pair in pairs:
+            number, color = pair.split(" ")
+            round_values[color] = int(number)
+
+        game_values.append(round_values)
+
+    return game_values
+
+
+def check_game(constraints: dict[str, int], game_values: list[dict[str, int]]) -> bool:
+    for color, value in constraints.items():
+        for game_value in game_values:
+            if (check := game_value.get(color)) is not None and check > value:
+                return False
+
+    return True
+
+
+def find_possibilities(lines: list[str]) -> int:
+    # only 12 red cubes, 13 green cubes, and 14 blue cubes
+    constraints = {
+        "red": 12,
+        "green": 13,
+        "blue": 14,
+    }
+
+    sum_of_ids: int = 0
+
+    for game_number, line in enumerate(lines, start=1):
+        game_values = parse_game(line)
+
+        if check_game(constraints, game_values):
+            sum_of_ids += game_number
+
+    return sum_of_ids
+
+
+@register_solution(2023, 2, 1)
+def part_one(input_data: list[str]):
+    answer = find_possibilities(input_data)
+
+    if not answer:
+        raise SolutionNotFoundError(2023, 2, 1)
+
+    return answer
+
+
+@register_solution(2023, 2, 2)
+def part_two(input_data: list[str]):
+    answer = ...
+
+    if not answer:
+        raise SolutionNotFoundError(2023, 2, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2023, 2)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2023/test_day_02_2023.py
@@ -0,0 +1,38 @@
+import pytest
+
+from adventofcode.year_2023.day_02_2023 import part_two, part_one, parse_game
+
+test_input = [
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+]
+
+
+@pytest.mark.parametrize(["game", "expected"], [
+    ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+     [{'blue': 3, 'red': 4}, {'blue': 6, 'green': 2, 'red': 1}, {'green': 2}]),
+    ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", [{'blue': 1, 'green': 2},
+                                                                          {'blue': 4, 'green': 3, 'red': 1},
+                                                                          {'blue': 1, 'green': 1}]),
+    ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", [{'blue': 6, 'green': 8, 'red': 20},
+                                                                                  {'blue': 5, 'green': 13, 'red': 4},
+                                                                                  {'green': 5, 'red': 1}]),
+    ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", [{'blue': 6, 'green': 1, 'red': 3},
+                                                                                  {'green': 3, 'red': 6},
+                                                                                  {'blue': 15, 'green': 3, 'red': 14}]),
+    ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+     [{'blue': 1, 'green': 3, 'red': 6}, {'blue': 2, 'green': 2, 'red': 1}]),
+])
+def test_parse_game(game, expected):
+    assert parse_game(game) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 8
+
+
+def test_part_two():
+    assert part_two(test_input) == 'x'
