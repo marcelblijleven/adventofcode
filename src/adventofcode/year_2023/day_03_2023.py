95aacc3c4cbcbc091fc72061ca838f2f07f7e7e8
Marcel Blijleven <marcelblijleven@gmail.com>
2023-12-04 20:23:10 +0100

    feat: add 2023 day 03

---
+++ src/adventofcode/year_2023/day_03_2023.py
@@ -0,0 +1,120 @@
+import re
+from typing import Iterable
+
+from adventofcode.util.exceptions import SolutionNotFoundError
+from adventofcode.registry.decorators import register_solution
+from adventofcode.util.input_helpers import get_input_for_day
+
+
+def find_symbol_coords(lines: list[str]) -> set[tuple[int, int]]:
+    """
+    Loop through all lines to find the x,y coordinates of all symbols
+    """
+    coords = set[tuple[int, int]]()
+
+    for y, line in enumerate(lines):
+        for x, value in enumerate(line):
+            if not value.isdigit() and value != ".":
+                coords.add((x, y))
+
+    return coords
+
+
+def find_gear_coords(lines: list[str]) -> dict[tuple[int, int], list[int]]:
+    """
+    Loop through all lines to find x,y coordinates of all gears (*)
+    """
+    coords: dict[tuple[int, int], []] = {}
+
+    for y, line in enumerate(lines):
+        for x, value in enumerate(line):
+            if value == "*":
+                coords[(x, y)] = []
+
+    return coords
+
+
+def neighbour_has_adjacent_symbol(current_position: tuple[int, int], symbols: set[tuple[int, int]]) -> bool:
+    x, y = current_position
+    for y_mod in [-1, 0, 1]:
+        for x_mod in [-1, 0, 1]:
+            _x = x + x_mod
+            _y = y + y_mod
+
+            if (x + x_mod, y + y_mod) in symbols:
+                return True
+
+    return False
+
+
+def neighbouring_gear_coords(current_position: tuple[int, int], gears: Iterable[tuple[int, int]]) -> tuple[int, int] | None:
+    x, y = current_position
+    for y_mod in [-1, 0, 1]:
+        for x_mod in [-1, 0, 1]:
+            gear_pos = x + x_mod, y + y_mod
+
+            if gear_pos in gears:
+                return gear_pos
+
+    return None
+
+
+def find_numbers(lines: list[str]) -> int:
+    symbols = find_symbol_coords(lines)
+    pattern = re.compile("\\d+")
+    total: int = 0
+
+    for y, line in enumerate(lines):
+        for match in pattern.finditer(line):
+            for x in range(match.start(), match.end()):
+                if neighbour_has_adjacent_symbol((x, y), symbols):
+                    total += int(match.group())
+                    break
+
+    return total
+
+
+def find_gear_ratios(lines: list[str]) -> int:
+    gears = find_gear_coords(lines)
+    pattern = re.compile("\\d+")
+    total: int = 0
+
+    for y, line in enumerate(lines):
+        for match in pattern.finditer(line):
+            for x in range(match.start(), match.end()):
+                if (gear_pos := neighbouring_gear_coords((x, y), gears)) is not None:
+                    if gear_pos not in gears:
+                        gears[gear_pos] = []
+                    gears[gear_pos].append(int(match.group()))
+
+                    if len(gears[gear_pos]) == 2:
+                        total += (gears[gear_pos][0] * gears[gear_pos][1])
+                    break
+
+    return total
+
+
+@register_solution(2023, 3, 1)
+def part_one(input_data: list[str]):
+    answer = find_numbers(input_data)
+
+    if not answer:
+        raise SolutionNotFoundError(2023, 3, 1)
+
+    return answer
+
+
+@register_solution(2023, 3, 2)
+def part_two(input_data: list[str]):
+    answer = find_gear_ratios(input_data)
+
+    if not answer:
+        raise SolutionNotFoundError(2023, 3, 2)
+
+    return answer
+
+
+if __name__ == '__main__':
+    data = get_input_for_day(2023, 3)
+    part_one(data)
+    part_two(data)
---
+++ tests/adventofcode/year_2023/test_day_03_2023.py
@@ -0,0 +1,59 @@
+import pytest
+
+from adventofcode.year_2023.day_03_2023 import (part_two, part_one, find_symbol_coords, find_gear_coords,
+                                                neighbour_has_adjacent_symbol, neighbouring_gear_coords)
+
+test_input = [
+    "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "617*......",
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "...$.*....",
+    ".664.598..",
+]
+
+
+def test_find_symbol_coords():
+    assert find_symbol_coords(test_input) == {(3, 8), (5, 5), (3, 4), (5, 8), (3, 1), (6, 3)}
+
+
+def test_find_gear_coords():
+    assert find_gear_coords(test_input) == {(3, 1): [], (3, 4): [], (5, 8): []}
+
+
+@pytest.mark.parametrize(["current_position", "expected"], [
+    ((0,0), True),
+    ((0,1), True),
+    ((1,1), True),
+    ((1,0), True),
+    ((2,2), True),
+    ((3,3), False),
+])
+def test_neighbour_has_adjacent_symbol(current_position, expected):
+    symbols = {(1, 1)}
+    assert neighbour_has_adjacent_symbol(current_position, symbols) == expected
+
+
+@pytest.mark.parametrize(["current_position", "expected"], [
+    ((0,0), (1, 1)),
+    ((0,1), (1, 1)),
+    ((1,1), (1, 1)),
+    ((1,0), (1, 1)),
+    ((2,2), (1, 1)),
+    ((3,3), None),
+])
+def test_neighbouring_gear_coords(current_position, expected):
+    gears = {(1, 1)}
+    assert neighbouring_gear_coords(current_position, gears) == expected
+
+
+def test_part_one():
+    assert part_one(test_input) == 4361
+
+
+def test_part_two():
+    assert part_two(test_input) == 467835
