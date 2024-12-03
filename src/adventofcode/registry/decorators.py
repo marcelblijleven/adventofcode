9328e1b0e93e50234623240d7009665c470783e9
Marcel Blijleven <marcelblijleven@gmail.com>
2024-12-03 21:00:54 +0100

    chore: add type hints to registry decorator

--- src/adventofcode/registry/decorators.py
+++ src/adventofcode/registry/decorators.py
@@ -1,14 +1,22 @@
+from collections.abc import Callable
 from functools import partial, wraps
+from typing import ParamSpec, TypeAlias, TypeVar
 
 from adventofcode.registry import registry
 from adventofcode.registry.util import get_registry_key
 from adventofcode.util.helpers import solution_timer
 
+R = TypeVar("R")
+P = ParamSpec("P")
+SolutionFunc: TypeAlias = Callable[P, R]
 
-def register_solution(year: int, day: int, part: int, version: str = "normal"):
+
+def register_solution(
+    year: int, day: int, part: int, version: str = "normal"
+) -> Callable[[SolutionFunc], SolutionFunc]:
     """Register a solution to the solution registry"""
 
-    def decorator(func):
+    def decorator(func: SolutionFunc) -> SolutionFunc:
         partial_func = partial(  # type: ignore
             solution_timer, func=func, year=year, day=day, part=part, version=version
         )
