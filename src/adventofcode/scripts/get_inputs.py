7e95e3e8dbbc73beee14c9198fabed907d49cb65
Marcel Blijleven <marcelblijleven@gmail.com>
2024-12-03 20:07:14 +0100

    fix: send user agent with input retrieval request

--- src/adventofcode/scripts/get_inputs.py
+++ src/adventofcode/scripts/get_inputs.py
@@ -1,25 +1,36 @@
+import json
 import os
+from typing import Required, TypedDict
 
 from httpx import get
 
 from adventofcode.config import ROOT_DIR
 
 
+class Setup(TypedDict):
+    user_agent: Required[str]
+    session_cookie: Required[str]
+
+
 def get_input(year: int, day: int):
-    session = _read_session()
-    data = _download_input(year, day, session)
+    """
+    Retrieves input from the Advent of Code website for the given year/day.
+    After retrieving the input, the input is stored in the project.
+    """
+    setup = _read_setup()
+    data = _download_input(year, day, setup)
     _save_input(data, year, day)
 
 
-def _download_input(year: int, day: int, session: str) -> bytes:
+def _download_input(year: int, day: int, setup: Setup) -> bytes:
     """
     Downloads the input as text from the advent of code site
     """
-    cookies = {"session": session}
+    cookies = {"session": setup["session_cookie"]}
     url = f"https://adventofcode.com/{year}/day/{day}/input"
-    resp = get(url, cookies=cookies)
+    resp = get(url, cookies=cookies, headers={"User-Agent": setup["user_agent"]})
     resp.raise_for_status()
-    return resp.content  # type: ignore
+    return resp.content
 
 
 def _save_input(data: bytes, year: int, day: int) -> None:
@@ -32,9 +43,15 @@ def _save_input(data: bytes, year: int, day: int) -> None:
         file.write(data)
 
 
-def _read_session():
-    target = os.path.join(ROOT_DIR, "../../.session")
+def _read_setup() -> Setup:
+    """
+    Reads .setup.json from the projects' root directory
+
+    Returns:
+        Setup: a typed dict
+    """
+    target = os.path.join(ROOT_DIR, "../../.setup.json")
     path = os.path.abspath(target)
 
     with open(path) as f:
-        return f.read()
+        return json.load(f)
